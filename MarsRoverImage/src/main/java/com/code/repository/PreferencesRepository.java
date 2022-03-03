76ca45c2752899e22be09a154e51bb6aec454db2
Redowan Shajib <39821409+rshajib@users.noreply.github.com>
2022-03-02 16:34:20 -0800

    Update PreferencesRepository.java

--- MarsRoverImage/src/main/java/com/code/repository/PreferencesRepository.java
+++ MarsRoverImage/src/main/java/com/code/repository/PreferencesRepository.java
@@ -5,8 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.code.dto.HomeDto;
 
 public interface PreferencesRepository extends JpaRepository<HomeDto, Long> {
-
-
   HomeDto findByUserId(Long userId);
-
 }
